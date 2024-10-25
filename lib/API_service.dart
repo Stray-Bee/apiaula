da9722f223025d04aabefdde353a18d7d069e6e6
Valhalla <valhallamaia@gmail.com>
2024-10-24 22:43:02 -0300

    PostMap

--- lib/API_service.dart
+++ lib/API_service.dart
@@ -1,3 +1,21 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:apiaula/post.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
   final String baseURL = 'https://jsonplaceholder.typicode.com/posts';
+
+  Future<List<Post>> getPosts() async{
+    final response = await http.get(Uri.parse(baseURL));
+    if(response.statusCode == 200){
+      List<dynamic> body = jsonDecode(response.body);
+      return body.map((dynamic item){
+        return Post.fromMap(item);
+      }).toList();
+    }
+    return[];
+  }
+  
 }
