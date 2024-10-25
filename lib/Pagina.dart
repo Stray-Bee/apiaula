a1ed6ad90be84af5c627ee7c38758e98ad57f449
Valhalla <valhallamaia@gmail.com>
2024-10-25 19:34:18 -0300

    _loadPosts

--- lib/Pagina.dart
+++ lib/Pagina.dart
@@ -1,3 +1,4 @@
+import 'package:apiaula/API_service.dart';
 import 'package:apiaula/post.dart';
 import 'package:flutter/material.dart';
 
@@ -9,17 +10,37 @@ class Pagina extends StatefulWidget {
 }
 
 class _PaginaState extends State<Pagina> {
+    final ApiService apiService = ApiService();
 
   List<Post> posts = [];
 
   @override
+  void initState() {
+    _loadPosts();
+    super.initState();
+
+  }
+
+  void _loadPosts() async{
+    var aux = await apiService.getPosts();
+    setState(() {
+      posts = aux;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+            title: const Text('API Exemplo'),
+            backgroundColor: Colors.deepPurple.shade100,
+        ),
       body: ListView.builder(
         itemCount: posts.length,
         itemBuilder: (context, index){
           return ListTile(
-            title: Text('${posts[index].userId}'),
+            title: Text('${posts[index].userId} - ${posts[index].id}'),
+            subtitle: Text ('${posts[index].title} - ${posts[index].body}')
           );  
         },
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Demo',
-      home: Pagina();
+      home: Pagina());
   }
 }
 
